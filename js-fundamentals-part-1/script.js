c8bf2042a1695c3c4e6312d999c14d2475dbefb5
Maik Cawelius <maikcawelius@web.de>
2024-03-18 15:59:29 +0100

    Add switch statement section

--- js-fundamentals-part-1/script.js
+++ js-fundamentals-part-1/script.js
@@ -530,6 +530,7 @@ if (favNumber === 25) {
 
 // if (favNumber !== 23) console.log("Why not 23?");
 
+/*
 // Basic Boolean Logic
 // A and B
 const hasDriversLicense = true; // A
@@ -555,3 +556,47 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
    console.log("Someone else should drive!");
 }
+*/
+
+/*
+const day = "sunday";
+// Switch statements do strict comparison
+switch(day) {
+   case "monday": // day === "monday"
+      console.log("Plan course structure.");
+      console.log("Go to coding meeting");
+      break;
+   case "tuesday":
+      console.log("Prepare theory videos");
+      break;
+   case "wednesday":
+   case "thursday":
+      console.log("Write code examples");
+      break;
+   case "friday":
+      console.log("Record videos.");
+      break;
+   case "saturday":
+   case "sunday":
+      console.log("Enjoy the weekend.");
+      break;
+   default:
+      console.log("Not a valid day.");
+}
+
+// Same using a switch statement
+if (day === "monday") {
+   console.log("Plan course structure.");
+   console.log("Go to coding meeting");
+} else if (day === "tuesday") {
+   console.log("Prepare theory videos");
+} else if ( day === "wednesday" || day === "thursday") {
+   console.log("Write code examples");
+} else if (day === "friday") {
+   console.log("Record videos.");
+} else if (day === "saturday" || day === "sunday") {
+   console.log("Enjoy the weekend.");
+} else {
+   console.log("Not a valid day");
+}
+*/
